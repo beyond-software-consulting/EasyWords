bbc09a16078af70997082a1d6a60e3e5035702de
Serhat Yalcin <serhatyalcin@gmail.com>
2019-05-03 20:47:55 +0300

    ...

--- Question.Test/QuestionTest.cs
+++ Question.Test/QuestionTest.cs
@@ -113,7 +113,6 @@ namespace Tests
         private static IConfiguration GetConfiguration()
         {
             var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddEnvironmentVariables();
 
